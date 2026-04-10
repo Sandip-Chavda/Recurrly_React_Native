019f372f79a29180e0acc0f7bfd5c2157d38d609
Sandip-Chavda <chavdas766@gmail.com>
2026-04-10 15:38:03 +0530

    Clerk authentication & Sign in inplemented

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,8 @@
 import { tabs } from "@/constants/data";
 import { colors, components } from "@/constants/theme";
+import { useAuth } from "@clerk/expo";
 import clsx from "clsx";
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import React from "react";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -9,7 +10,18 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 const tabbar = components.tabBar;
 
 const TabsLayout = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
   const insets = useSafeAreaInsets();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  // Redirect to sign-in if user is not authenticated
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)/sign-in" />;
+  }
 
   const TabIcon = ({ focused, icon }: TabIconProps) => {
     return (
